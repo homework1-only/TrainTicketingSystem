d27c2fc347fe0bef07d74669a12b21c29cddc261
kvon <1109042859@qq.com>
2022-10-23 22:42:53 +0800

    项目没问题

--- train-user/src/router/index.js
+++ train-user/src/router/index.js
@@ -1,24 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
 import Index from '../views/Index.vue'
-import MyOrder from '../views/MyOrder.vue'
 import MyServe from '../views/Serve.vue'
-import About from '../views/About.vue'
 import Mine from '../views/Mine.vue'
 import Citylist from '../views/CityList.vue'
 import Querytrain from '../views/QueryTrain.vue'
 import Setting from "../views/Setting";
 import ConfirmOrder from '../views/ConfirmOrder.vue'
 import SelectUser from '../views/SelectUser.vue'
-import AddUser from '../views/AddUser.vue'
 import AddUser from '../views/addUser.vue'
 import MyOrder from "../views/MyOrder";
 import Serve from "../views/Serve";
 import About from "../views/About";
-// import DetailOrder from '../views/DetailOrder.vue'
-// import ChangeTicket from '../views/ChangeTicket.vue'
+import Register from "../views/Register";
 
 Vue.use(VueRouter)
 
@@ -53,16 +48,7 @@ const routes = [
         name: 'selectuser',
         component: SelectUser
     },
-    // {
-    //     path: '/detailorder',
-    //     name: 'detailorder',
-    //     component: DetailOrder
-    // },
-    // {
-    //     path: '/changeticket',
-    //     name: 'changeticket',
-    //     component: ChangeTicket
-    // },
+
     {
         path: '/index',
         name: 'index',
@@ -83,12 +69,6 @@ const routes = [
         name: 'myServe',
         component: MyServe,
     },
-    },
-    //{
-    //     path: '/order',
-    //     name: 'order',
-    //     component: Order,
-    // },
     {
         path: '/mine',
         name: 'mine',
@@ -120,7 +100,6 @@ const routes = [
         component: AddUser,
     }
 ]
-
 
 
 const router = new VueRouter({
